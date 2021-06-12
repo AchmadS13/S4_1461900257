61a2f8ffa62a04536760258302fe742cd16f5c70
AchmadS13 <achmadsuhaimi1@gmail.com>
2021-06-12 16:49:50 +0700

    pertemuan4

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\KelasController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\DokterController;
 
-Route::get("/", [KelasController::class, 'index']);
-Route::get("export", [KelasController::class, 'Export']);
+
+Route::get('/', [DokterController::class, 'index']);
+Route::post("dokter-import", [DokterController::class, 'import'])->name("dokter.import");
+Route::get('/aboutme', function () {
+    return view('aboutme');
+});
+Route::get('/artikel', function () {
+    return view('artikel');
+});
